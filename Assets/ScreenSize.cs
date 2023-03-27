246ebf2434906c4252bd09c7ade0f122c6080588
LaihoJuhani <laihojuhai@gmail.com>
2023-03-27 19:40:19 +0300

    Add: Basgaa

---
+++ Assets/ScreenSize.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScreenSize : MonoBehaviour
+{   
+    private int screenHeight;
+    private int screenWidth;
+    private Rect safe;
+
+    void Start()
+    {
+        screenHeight = Screen.height;
+        screenWidth = Screen.width;
+        safe = Screen.safeArea;
+        Debug.Log(screenWidth);
+        Debug.Log(screenHeight);
+        Debug.Log(safe);
+    }
+}
